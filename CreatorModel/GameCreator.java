3e311ee2888e64c0929ef9276e6f81b2b641483d
Fariha <fariha@Guest1s-MacBook-Pro.local>
2023-11-08 23:25:58 -0500

    game choice buttons

--- CreatorModel/GameCreator.java
+++ CreatorModel/GameCreator.java
@@ -1,11 +1,13 @@
 package CreatorModel;
 
+import GameModel.AccessibleGame;
+import Visualizer.GameVisualizer;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
-import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 public class GameCreator {
@@ -21,17 +23,67 @@ public class GameCreator {
         Scene scene = new Scene(root, Color.BLACK);
 
         Text text = new Text("CHOOSE YOUR GAME");
-        text.setX(135);
+        text.setX(140);
         text.setY(100);
         text.setFont(Font.font("Arial", 30));
         text.setFill(Color.WHITE);
         root.getChildren().add(text);
+
+        Button tinyGame = new Button("TinyGame");
+        tinyGame.setLayoutX(150);
+        tinyGame.setLayoutY(170);
+        tinyGame.setPrefWidth(300);
+        tinyGame.setPrefHeight(70);
+        tinyGame.setStyle("-fx-background-color: green; -fx-text-fill: white; -fx-font-size: 20px;");
+        root.getChildren().add(tinyGame);
+        gameButtonHandler(tinyGame);
+
+        Button mediumGame = new Button("MediumGame");
+        mediumGame.setLayoutX(150);
+        mediumGame.setLayoutY(270);
+        mediumGame.setPrefWidth(300);
+        mediumGame.setPrefHeight(70);
+        mediumGame.setStyle("-fx-background-color: green; -fx-text-fill: white; -fx-font-size: 20px;");
+        root.getChildren().add(mediumGame);
+        gameButtonHandler(mediumGame);
+
+        Button createGame = new Button("Create Your Own Game");
+        createGame.setLayoutX(150);
+        createGame.setLayoutY(370);
+        createGame.setPrefWidth(300);
+        createGame.setPrefHeight(70);
+        createGame.setStyle("-fx-background-color: green; -fx-text-fill: white; -fx-font-size: 17px;");
+        root.getChildren().add(createGame);
+        createButtonHandler(createGame);
 
         stage.setScene(scene);
         stage.setTitle("Choose your Accessible Game");
         stage.setWidth(600);
         stage.setHeight(600);
         stage.show();
+    }
+
+    public void gameButtonHandler(Button gameButton) {
+        gameButton.setOnMousePressed(event -> gameButton.setStyle("-fx-background-color: orange; -fx-text-fill: white; -fx-font-size: 17px;"));
+        gameButton.setOnMouseReleased(event -> gameButton.setStyle("-fx-background-color: green; -fx-text-fill: white; -fx-font-size: 17px;"));
+        gameButton.setOnAction(event -> {
+            AccessibleGame model = new AccessibleGame(gameButton.getText());
+            GameVisualizer visualizer = new GameVisualizer(model, stage);
+        });
+    }
+
+    public void createButtonHandler(Button gameButton) {
+        gameButton.setOnMousePressed(event -> gameButton.setStyle("-fx-background-color: orange; -fx-text-fill: white; -fx-font-size: 17px;"));
+        gameButton.setOnMouseReleased(event -> gameButton.setStyle("-fx-background-color: green; -fx-text-fill: white; -fx-font-size: 17px;"));
+        gameButton.setOnAction(event -> {
+            creatingPage();
+        });
+    }
+
+    public void creatingPage() {
+        Group root2 = new Group();
+        Scene scene2 = new Scene(root2, Color.DARKGREEN);
+        stage.setScene(scene2);
     }
 
 }
