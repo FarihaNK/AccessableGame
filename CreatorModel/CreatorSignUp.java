794b2931d41be9a77a914db27180bf1e2da6c40c
FariaNK <nawed.khan7904@gmail.com>
2023-11-21 02:16:51 -0500

    signin page

---
+++ CreatorModel/CreatorSignUp.java
@@ -0,0 +1,110 @@
+package CreatorModel;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.AccessibleRole;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class CreatorSignUp {
+    Stage stage;
+    Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/GameCreatorApp", "root", "Thebookthief100%");
+    public CreatorSignUp(Stage stage) throws SQLException {
+        this.stage = stage;
+        runUI();
+    }
+
+    public void runUI(){
+        VBox root = new VBox();
+        root.setAlignment(Pos.CENTER);
+        root.setSpacing(30);
+        root.setStyle("-fx-background-color: darkorange;");
+        Scene scene = new Scene(root);
+
+        Button signUpButton = new Button("Sign Up");
+        signUpButton.setPrefWidth(300);
+        signUpButton.setPrefHeight(100);
+        signUpButton.setStyle("-fx-background-color: green; -fx-text-fill: black; -fx-font-size: 25px;");
+        root.getChildren().add(signUpButton);
+        signUpHandler(signUpButton);
+
+        Button logInButton = new Button("Log In");
+        logInButton.setPrefWidth(300);
+        logInButton.setPrefHeight(100);
+        logInButton.setStyle("-fx-background-color: green; -fx-text-fill: black; -fx-font-size: 25px;");
+        root.getChildren().add(logInButton);
+
+        stage.setScene(scene);
+        stage.setTitle("SignIn");
+        stage.setWidth(600);
+        stage.setHeight(600);
+        stage.show();
+    }
+
+    public void signUpHandler(Button button){
+        button.setOnMousePressed(event -> button.setStyle("-fx-background-color: white; -fx-text-fill: black; -fx-font-size: 25px;"));
+        button.setOnMouseReleased(event -> button.setStyle("-fx-background-color: green; -fx-text-fill: black; -fx-font-size: 25px;"));
+        button.setOnAction(event -> {
+            signInPage();
+        });
+    }
+    public void signInPage(){
+        VBox root = new VBox();
+        root.setAlignment(Pos.CENTER);
+        root.setSpacing(30);
+        root.setPadding(new Insets(100,100,100,100));
+        root.setStyle("-fx-background-color: white;");
+        Scene scene = new Scene(root);
+
+        Text text = new Text("Sign Up");
+        text.setFont(Font.font("Arial", 30));
+        text.setFill(Color.BLACK);
+        root.getChildren().add(text);
+
+        Label usernameLabel = new Label("Username");
+        usernameLabel.setFont(Font.font("Arial", 20));
+        usernameLabel.setTextFill(Color.BLACK);
+        root.getChildren().add(usernameLabel);
+
+        TextField usernameTextfeild = new TextField("Type here");
+        root.getChildren().add(usernameTextfeild);
+        usernameTextfeild.setAccessibleRole(AccessibleRole.TEXT_AREA);
+        usernameTextfeild.setFont(new Font("Arial", 20));
+        usernameTextfeild.setFocusTraversable(true);
+        usernameTextfeild.setPrefHeight(50);
+        usernameTextfeild.setStyle("-fx-background-color: lightgreen;");
+
+        Label passwordLabel = new Label("Password");
+        passwordLabel.setFont(Font.font("Arial", 20));
+        passwordLabel.setTextFill(Color.BLACK);
+        root.getChildren().add(passwordLabel);
+
+        TextField passwordTextfeild = new TextField("Type here");
+        root.getChildren().add(passwordTextfeild);
+        passwordTextfeild.setAccessibleRole(AccessibleRole.TEXT_AREA);
+        passwordTextfeild.setFont(new Font("Arial", 20));
+        passwordTextfeild.setFocusTraversable(true);
+        passwordTextfeild.setPrefHeight(50);
+        passwordTextfeild.setStyle("-fx-background-color: lightgreen;");
+
+        Button submitButton = new Button("Submit");
+        submitButton.setPrefWidth(200);
+        submitButton.setPrefHeight(60);
+        submitButton.setStyle("-fx-background-color: orange; -fx-text-fill: black; -fx-font-size: 17px;");
+        root.getChildren().add(submitButton);
+
+
+        stage.setScene(scene);
+    }
+}
--- GameCreatorApp.java
+++ GameCreatorApp.java
@@ -1,3 +1,4 @@
+import CreatorModel.CreatorSignUp;
 import CreatorModel.GameCreator;
 import javafx.application.Application;
 import javafx.stage.Stage;
@@ -7,12 +8,14 @@ import java.sql.*;
 public class GameCreatorApp extends Application{
 
         GameCreator creator;
+        CreatorSignUp signUp;
 public static void main(String[]args){
         launch(args);
         }
 
 @Override
 public void start(Stage stage) throws SQLException {
-        this.creator = new GameCreator(stage);
+        //this.creator = new GameCreator(stage);
+        this.signUp = new CreatorSignUp(stage);
         }
 }
